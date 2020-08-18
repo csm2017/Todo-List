d850e413adb67639c3d8b0aa460e53a1b6429c9c
csm2017 <mcgahancory@gmail.com>
2020-08-18 16:24:47 -0500

    styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,22 @@
 // Check off todos by clicking
-$('li').click(function(){
+$('ul').on('click', 'li', function(){
     $(this).toggleClass('completed');
 });
 
 // click on X to delete todo
-$('span').click(function(event){
+$('ul').on('click', 'span', function(event){
     $(this).parent().fadeOut(500, function(){
         //'this' refers to the selected li parent now
         $(this).remove()
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13 && $(this).val() !== '') {
+        var todoText = $(this).val();
+        $(this).val("");
+        //create new li and add to ul
+        $('ul').append(`<li><span>X</span> ${todoText}</li>`);
+    }
 });
