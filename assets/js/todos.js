4b6f4a84f2447fca9315397a818e4a82c4a06649
csm2017 <mcgahancory@gmail.com>
2020-08-18 16:52:52 -0500

    more styling and fixed a bug with fontawesome icons

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -13,10 +13,15 @@ $('ul').on('click', 'span', function(event){
 });
 
 $("input[type='text']").keypress(function(event){
-    if(event.which === 13 && $(this).val() !== '') {
-        var todoText = $(this).val();
-        $(this).val("");
-        //create new li and add to ul
-        $('ul').append(`<li><span>X</span> ${todoText}</li>`);
-    }
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
 });
